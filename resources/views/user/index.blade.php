ed33e7a818949af1bcb7bf518ff613758bc56fb9
Iqbal Hamsyah <iqbalhamsyah@gmail.com>
2024-09-01 15:44:36 +0700

    ngubah breadcrumbs

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('subtitle', 'Welcome')
 @section('content_header_title', 'Pegawai')
-@section('content_header_subtitle', 'Index')
+@section('content_header_subtitle', 'Data Pegawai')
 
 {{-- Content body: main page content --}}
 @php
